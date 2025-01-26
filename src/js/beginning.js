c92eb19e0af2d56f701c6c392855907b90adae12
Fabrizio Franco <fabrizio.software.dev@gmail.com>
2025-01-26 14:20:50 -0400

    Sacar index a la raíz del proyecto

--- src/js/beginning.js
+++ src/js/beginning.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header');
-    const menuToggle = document.querySelector('.menu-toggle');
-    const mobileNav = document.querySelector('.mobile-nav');
+    const burguerButton = document.getElementById('burguer-button');
+    const closeButton = document.getElementById('close-button');
+    const mobilNav = document.querySelector('.mobile-nav');
+    const navContent = document.querySelector('.nav-content');
 
     window.addEventListener('scroll', () => {
         if (window.scrollY > 10) {
@@ -13,7 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    menuToggle.addEventListener('click', () => {
-        mobileNav.classList.toggle('open');
-    });
+    burguerButton.addEventListener('click', () => {
+        mobilNav.classList.add('open');
+        navContent.style.visibility = "hidden";
+    })
+
+    closeButton.addEventListener('click', () => {
+        mobilNav.classList.remove('open');
+        navContent.style.visibility = "visible";
+    })
 });
